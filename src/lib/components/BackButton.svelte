0a1a6cb2bef53a946861c87181240aff8293c21f
jaftdelgado <jaftdelgado@gmail.com>
2026-03-19 18:42:10 -0600

    feat(landing): add basic hero section with typefaces assets

---
+++ src/lib/components/BackButton.svelte
@@ -0,0 +1,23 @@
+<script>
+	let { href = '' } = $props();
+</script>
+
+<button
+	class="group relative flex h-12 items-center rounded-2xl bg-slate-50 pr-6 pl-16 font-semibold text-black shadow-xs transition-all duration-300"
+	type="button"
+	onclick={() => href && (window.location.href = href)}
+>
+	<div
+		class="absolute top-1 left-1 z-10 flex size-10 items-center justify-center rounded-xl bg-slate-900 duration-500 group-hover:w-[calc(100%-8px)]"
+	>
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" height="25px" width="25px">
+			<path d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z" fill="#ffffff" />
+			<path
+				d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"
+				fill="#ffffff"
+			/>
+		</svg>
+	</div>
+
+	<span class="font-sans text-base whitespace-nowrap"> Explorar propiedades </span>
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import BackButton from '$lib/components/BackButton.svelte';
+</script>
+
+<section class="flex min-h-[80vh] flex-col items-center justify-center bg-white px-4 text-center">
+	<div class="max-w-2xl">
+		<h1
+			class="font-sans text-4xl font-light tracking-tight text-neutral-950 sm:text-5xl md:text-6xl lg:text-6xl"
+		>
+			El
+			<span class="font-serif text-neutral-800 italic">lujo</span>
+			de encontrar tu lugar en el
+			<span class="font-serif text-neutral-800 italic">mundo</span>
+		</h1>
+	</div>
+
+	<p class="mt-6 max-w-xl font-sans text-base text-neutral-600 md:text-lg">
+		Descubre espacios diseñados para conectar con tu esencia. Arquitectura, diseño y confort en
+		armonía con tu estilo de vida.
+	</p>
+
+	<div class="mt-10">
+		<BackButton href="/propiedades" />
+	</div>
+</section>
